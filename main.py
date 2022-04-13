5a81c0039c4591d9f581c791fc45929e5d5fa92b
jesperglas <jesper.glas@gmail.com>
2022-04-13 17:15:59 +0200

    Webcam image showing.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import cv2
 
 TITLE : str = 'Face Tracking'
 VERSION : str = '1.0.0'
-    
+
+# Inits the layout of the window
 def initLayout() -> list:
     return [
         [psg.Image(key = '-IMAGE-')],
@@ -15,13 +16,27 @@ def initLayout() -> list:
 def run():
     layout : list = initLayout()
     
+    # Init window
     window = psg.Window(f'{TITLE} - v{VERSION}', layout)
 
+    # Init video capture
+    video = cv2.VideoCapture(0) # 0 = webcam
+    face_cascade = cv2.CascadeClassifier('./assets/opencv_cascades/haarcascade_frontalface_default.xml')
+
+    # Main loop
     while True:
-        event, values = window.read()
+        event, values = window.read(timeout = 0)
         if event == psg.WIN_CLOSED:
             break
         
+        # Video
+        arg, frame = video.read()
+
+        # Update the image
+        imgbytes = cv2.imencode('.png', frame)[1].tobytes() # Index 1 is image data
+        window['-IMAGE-'].update(data = imgbytes)
+    
+    # Shutdown
     window.close()
 
 # run
